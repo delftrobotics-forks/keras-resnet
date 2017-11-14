0edc0271c34d9184a5b26e1b0699e780641ff3a6
Enrico Liscio <e.liscio@fizyr.com>
2017-11-14 17:20:58 +0100

    Changed name to first conv layer.

--- keras_resnet/models/_2d.py
+++ keras_resnet/models/_2d.py
@@ -62,7 +62,7 @@ def ResNet(inputs, blocks, block, include_top=True, classes=1000, freeze_bn=True
         numerical_names = [True] * len(blocks)
 
     x = keras.layers.ZeroPadding2D(padding=3, name="padding_conv1")(inputs)
-    x = keras.layers.Conv2D(64, (7, 7), strides=(2, 2), use_bias=False, name="conv1")(x)
+    x = keras.layers.Conv2D(64, (7, 7), strides=(2, 2), use_bias=False, name="conv_1")(x)
     x = keras_resnet.layers.BatchNormalization(axis=axis, epsilon=1e-5, freeze=freeze_bn, name="bn_conv1")(x)
     x = keras.layers.Activation("relu", name="conv1_relu")(x)
     x = keras.layers.MaxPooling2D((3, 3), strides=(2, 2), padding="same", name="pool1")(x)
